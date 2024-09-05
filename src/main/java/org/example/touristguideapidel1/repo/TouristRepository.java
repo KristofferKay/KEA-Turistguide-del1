45df74fc107231a1f268a1e11d8ce5815931034e
Kristoffer Kay <37834268+KristofferKay@users.noreply.github.com>
2024-09-05 07:58:14 +0200

    Added HTML for part 7

--- src/main/java/org/example/touristguideapidel1/repo/TouristRepository.java
+++ src/main/java/org/example/touristguideapidel1/repo/TouristRepository.java
@@ -12,6 +12,9 @@ import java.util.List;
 
 @Repository
 public class TouristRepository {
+
+    //TODO add expections with HTTPS status codes instead of NULLs where it returns
+
     private List<TouristAttraction> touristAttractions = new ArrayList<>();
 
     public TouristRepository() {
@@ -19,9 +22,10 @@ public class TouristRepository {
     }
 
     public void populate() {
-        touristAttractions.add(new TouristAttraction("Attraction1", "desc1"));
-        touristAttractions.add(new TouristAttraction("Attraction2", "desc2"));
-        touristAttractions.add(new TouristAttraction("Attraction3", "desc3"));
+        touristAttractions.add(new TouristAttraction("Eiffel Tower", "An iconic wrought-iron lattice tower in Paris, France."));
+        touristAttractions.add(new TouristAttraction("Great Wall of China", "A series of fortifications made of stone, brick, and other materials, built along the northern borders of China."));
+        touristAttractions.add(new TouristAttraction("Statue of Liberty", "A colossal neoclassical sculpture on Liberty Island in New York Harbor, USA."));
+        touristAttractions.add(new TouristAttraction("Colosseum", "An ancient amphitheater in Rome, Italy, known for its grandeur and history."));
     }
 
 
